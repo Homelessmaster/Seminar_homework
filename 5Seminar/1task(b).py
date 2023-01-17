832f2c8de6c905d7b007d93c54fbb939bdd7ff4e
Julian <maksimovyou@gmail.com>
2023-01-17 20:29:49 +0900

    1 version

--- 5Seminar/1task(b).py
+++ 5Seminar/1task(b).py
@@ -1,5 +1,3 @@
-import random
-
 def check(quantity, name, bank):
 
     quantity = int(input(f"{name}, введите количество конфет, которую забираете \n>>> "))
@@ -18,15 +16,17 @@ def check(quantity, name, bank):
     return quantity
 
 
-def checkBot(quantity, bank):
+def checkBot(quantity, bank, userCandy, cheat):
+    
+    if(bank > cheat):
+        quantity = bank - cheat
+        return quantity
+    
+    quantity = 29 - userCandy 
 
-    quantity = random.randint(1, 29)
+    if(bank - quantity < 0):
+        quantity = bank
 
-    while True:
-        if(bank - quantity < 0):
-            quantity = random.randint(1, bank)
-        else:
-            break     
     return quantity
 
 
@@ -37,9 +37,12 @@ def name(user):
 
 CandyBank = 500
 CandyUser1 = 0
-CandyUser2 = 0
+CandyBot = 0
 NameUser1 = 'user1'
 NameBot = 'Alex'
+Botcheat = CandyBank - CandyBank % 29  
+
+
 
 
 
@@ -50,23 +53,28 @@ print(f"Банк конфет {CandyBank}")
 print("Вы можете взять не менее 1 конфеты и не более 28")
 print("Игра начинается!")
 
-while(CandyBank > 0):
-
-    CandyUser1 = check(CandyUser1, NameUser1, CandyBank)
-
-    if(CandyBank - CandyUser1 == 0):
-        print(f"Попедил {NameUser1}!")
-        break
-
-    CandyBank = CandyBank - CandyUser1
-    print(f"Осталось конфет: {CandyBank}")
 
 
-    CandyUser2 = checkBot(CandyUser2, CandyBank)
+while True:
+    while(CandyBank > 0):
 
-    if(CandyBank - CandyUser2 == 0):
-        print(f"Попедил {NameBot}!")
-        break
+        CandyBot = checkBot(CandyBot, CandyBank, CandyUser1, Botcheat)
 
-    CandyBank = CandyBank - CandyUser2
-    print(f"{NameBot} сделал ход.\nОсталось конфет: {CandyBank}")
+        if(CandyBank - CandyBot == 0):
+            print(f"Победил {NameBot}!")
+            break
+
+        CandyBank = CandyBank - CandyBot
+        print(f"{NameBot} сделал ход.\nОсталось конфет: {CandyBank}")
+
+
+        CandyUser1 = check(CandyUser1, NameUser1, CandyBank)
+
+        if(CandyBank - CandyUser1 == 0):
+            print(f"Победил {NameUser1}!")
+            break
+
+        CandyBank = CandyBank - CandyUser1
+        print(f"Осталось конфет: {CandyBank}")
+    
+
